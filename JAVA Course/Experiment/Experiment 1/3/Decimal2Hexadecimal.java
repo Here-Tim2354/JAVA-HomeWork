9d6be4e452d0f82714e64acee4b59b8f539d0d1b
Here-Tim2354 <2523071971@qq.com>
2025-10-11 18:35:42 +0800

    完成练习2.1

---
+++ JAVA Course/Experiment/Experiment 1/3/Decimal2Hexadecimal.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Decimal2Hexadecimal {
+    private final int decimal;
+    private String hexadecimal;
+
+    public Decimal2Hexadecimal(int number){
+        if (number < 0) {
+            throw new IllegalArgumentException("输入不能为负数!");
+        }
+        decimal = number;
+    }
+
+    public void processor(){
+        try {
+            hexadecimal = String.format("%X", decimal);
+            System.out.println("输出结果为：" + hexadecimal);
+        } catch (Exception e){
+            System.out.println("转换错误: " + e.getMessage());
+            hexadecimal = "0";
+        }
+    }
+
+    public String getHexadecimal() {
+        return hexadecimal;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("请输入十进制数，且不能为负数：");
+            Decimal2Hexadecimal d2hClass = new Decimal2Hexadecimal(sc.nextInt());
+            d2hClass.processor();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
+    }
+}
---
+++ JAVA Course/JAVA Practice/Partice 2/1/Rectangle.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Rectangle {
+    double width;
+    double height;
+
+    public Rectangle(double width,double height){
+        this.width=width;
+        this.height=height;
+    }
+
+    public double getPerimeter(){c
+        return (width+height)*2;
+    }
+    public double getArea(){
+        return width*height;
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入矩形的宽度和高度：");
+        double width=sc.nextInt();
+        double weight=sc.nextInt();
+        Rectangle rectangle=new Rectangle(width,weight);
+        System.out.println("矩形周长: " + rectangle.getPerimeter());
+        System.out.println("矩形面积: " + rectangle.getArea());
+        sc.close();
+    }
+}
