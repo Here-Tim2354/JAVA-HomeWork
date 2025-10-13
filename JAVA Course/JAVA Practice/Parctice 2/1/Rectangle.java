b286905b5c7f3d8b3c1bf4bc99730b8dab5dab9b
Here-Tim2354 <2523071971@qq.com>
2025-10-13 20:55:23 +0800

    修正文件夹

--- JAVA Course/JAVA Practice/Parctice 2/1/Rectangle.java
+++ JAVA Course/JAVA Practice/Parctice 2/1/Rectangle.java
@@ -8,7 +8,7 @@ public class Rectangle {
         this.height=height;
     }
 
-    public double getPerimeter(){c
+    public double getPerimeter(){
         return (width+height)*2;
     }
     public double getArea(){
