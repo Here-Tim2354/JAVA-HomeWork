8b809e2c9a104680998f9eef0ca8a5f9716898bc
Here-Tim2354 <2523071971@qq.com>
2025-10-31 17:50:10 +0800

    提交练习5.1（未完成）

---
+++ JAVA_Course/Practice/Practice5/task1/Location.java
@@ -0,0 +1,69 @@
+package Practice.Practice5.task1;
+
+public class Location<T extends Comparable<T>> {
+    private int row;
+    private int column;
+    private T value;
+    
+    public Location() {
+    }
+    
+    public Location(int row, int column, T value) {
+        this.row = row;
+        this.column = column;
+        this.value = value;
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public void setRow(int row) {
+        this.row = row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString(){
+        return "Location[row="+row+", column="+column+", value="+value+"]";
+    }
+
+    public static <E extends Comparable<E>> Location<E> findMax(E[][] matrix){
+        if(matrix==null||matrix.length==0||matrix[0].length==0){
+            return null;
+        }
+        int maxRow = 0;
+        int maxCol = 0;
+        E maxValue = matrix[0][0];
+        
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j].compareTo(maxValue) > 0){
+                    maxValue = matrix[i][j];
+                    maxRow = i;
+                    maxCol = j;
+                }
+            }
+        }
+        
+        return new Location<E>(maxRow, maxCol, maxValue);
+    }
+
+    public static <E extends Comparable<E>> Location<E> findMin(E[][] matrix){
+        if(matrix==null||matrix.length==0||matrix[0].length==0){
+            return null;
+        }
+
+    }
+}
